395342291c8fad9d445633e4ab7d571a1d4c5fa8
七叶草 <609087282@qq.com>
2023-10-25 21:39:01 +0800

    Initial commit

---
+++ src/test/kotlin/KtBase01.kt
@@ -0,0 +1,16 @@
+//package
+
+// Todo  kotlin语言声明变量与内置数据类型
+fun main(){
+    println("hello world")
+
+    // todo =========声明变量
+    /*
+    可读可写    变量名     类型      值
+    var        name  :  String = "Derry"
+     */
+
+    var name : String = "Derry"
+    name = "七叶草"
+    println(name)
+}
