dfc46157aa12e0e4ae77f93155f91039feb103e9
七叶草 <609087282@qq.com>
2023-10-25 21:45:44 +0800

    02

--- src/test/kotlin/KtBase01.kt
+++ src/test/kotlin/KtBase01.kt
@@ -13,4 +13,18 @@ fun main(){
     var name : String = "Derry"
     name = "七叶草"
     println(name)
+
+    // TODO ==============内置数据类型
+    /*
+    这些都是 Kotlin的类
+    String      字符串
+    Char        单字符
+    Boolean     true/false
+    Int         整型           ---->最终会转成 java 的 int
+    Float       浮点型         ---->最终会转成  java 的 float
+    Double      双精度 小数
+    List        集合
+    Set         无重复的元素集合
+    Map         键值对的集合
+     */
 }
